e2be7d6adb90cf5956f84a02c98a16a1093e2d34
MalachiNoel3 <52811629+MalachiNoel3@users.noreply.github.com>
2025-06-10 10:34:02 -0500

    does this fix

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js"
+
 var firebaseConfig = {
     apiKey: "AIzaSyBhZD91sAHQJx-OWhpTAcV1KDWusDnb9p4",
     authDomain: "minerals-eeba4.firebaseapp.com",
